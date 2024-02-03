93c081ac84f0f55dab496b8c2288ca84c8f11061
zachmshort <zach.short@fantomworks.com>
2024-02-03 09:18:47 -0500

    put contents of app.js into home.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,8 @@ import React, { useState, useEffect }  from 'react';
 // import Nav from './components/NavTabs';
 
 function App() {
-  const [showHomePage, setShowHomePage] = useState(true);
-
-  useEffect(() => {
-  }, []);
-
-  const handleNavigate = () => {
-    setShowHomePage(false);
-  };
-
   return (
     <body>
-      <div className="myName-Info">
-      <h1 className="myName">Hi! I'm Zachary Short</h1>
-      <p className="meDesc">I have a passion for UI/UX design, 3D modeling/animation, and video editing</p>
-      </div>
-        <ul className="ulHomePage">
-          <li><Link to="/" className="link" onClick={handleNavigate}>Home</Link></li>
-          <li><Link to="/Websites" className="link" onClick={handleNavigate}>Websites I've Built</Link></li>
-          <li><Link to="/Models" className="link" onClick={handleNavigate}>3D Models I've Built</Link></li>
-          <li><Link to="/Videos" className="link" onClick={handleNavigate}>Videos I've Edited</Link></li>
-          <li><Link to="/Contact" className="link" onClick={handleNavigate}>Contact Me</Link></li>
-          <li><Link to="/About"className="link" onClick={handleNavigate}>About Me</Link></li>
-        </ul>
         <Outlet />
     </body>
     
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,32 @@
+
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect }  from 'react';
+
 export default function Home() {
+  const [showHomePage, setShowHomePage] = useState(true);
+
+  useEffect(() => {
+  }, []);
+
+  const handleNavigate = () => {
+    setShowHomePage(false);
+  };
+
     return (
       <>
-      </>
+      <div className="myName-Info">
+      <h1 className="myName">Hi! I'm Zachary Short</h1>
+      <p className="meDesc">I have a passion for UI/UX design, 3D modeling/animation, and video editing</p>
+      </div>
+        <ul className="ulHomePage">
+          <li><Link to="/" className="link" onClick={handleNavigate}>Home</Link></li>
+          <li><Link to="/Websites" className="link" onClick={handleNavigate}>Websites I've Built</Link></li>
+          <li><Link to="/Models" className="link" onClick={handleNavigate}>3D Models I've Built</Link></li>
+          <li><Link to="/Videos" className="link" onClick={handleNavigate}>Videos I've Edited</Link></li>
+          <li><Link to="/Contact" className="link" onClick={handleNavigate}>Contact Me</Link></li>
+          <li><Link to="/About"className="link" onClick={handleNavigate}>About Me</Link></li>
+        </ul>
+        </>
     );
   }
   
